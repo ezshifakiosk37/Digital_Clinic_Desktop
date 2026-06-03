e20c593b4611c2f24dc02c00aa3caa52b882eb05
AsharMeraj <ezshifakiosk37@gmail.com>
2026-06-03 18:22:02 +0500

    Adding pop up on sugar value

--- app/dashboard/vitals/_components/RapidTestingPage.tsx
+++ app/dashboard/vitals/_components/RapidTestingPage.tsx
@@ -279,7 +279,7 @@ const RapidTestingPage: React.FC<RapidTestingPageProps> = ({
         <div className="min-h-screen bg-slate-50">
             {/* Toast popup */}
             <ToastPopup
-                visible={true}
+                visible={showGlucosePopup}
                 glucoseValue={lastGlucoseValue}
                 onDismiss={() => setShowGlucosePopup(false)}
             />
